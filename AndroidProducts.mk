2ebb551a5162d1edbe122947ed33aef6a6fda4d9
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2023-03-25 10:37:34 +0100

    M6763D: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm64 Manufacturer: oppo Model: OPPO A83js Script version: 3.0.0

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_M6763D.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_M6763D-user \
+    omni_M6763D-userdebug \
+    omni_M6763D-eng
